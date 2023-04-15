ede99e82d928200e41f4fb5200858b1cb2ddfb6f
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-15 21:49:13 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -6,4 +6,5 @@
 
     <x-tests.card title="タイトル１" content="本文１" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル２"></x-test.card>
+    <x-tests.card title="CSSを変更したい" class="bg-red-50"></x-tests.card>
 </x-tests.app>
