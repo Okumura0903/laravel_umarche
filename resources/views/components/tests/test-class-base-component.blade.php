78cf974568ccc2425477d9300c1b250875e02351
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-15 22:04:05 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントテストです
+    <div>{{$classBaseMessage}}</div>
+    <div>{{$defaultMessage}}</div>
+    <!-- When there is no desire, all things are at peace. - Laozi -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,5 +2,9 @@
     <x-slot name="header">
         ヘッダー２
     </x-slot>
-コンポーネントテスト２
+    コンポーネントテスト２
+    <x-test-class-base classBaseMessage="メッセージです"></x-test-class-base>
+    <div class="mb-4">
+        <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています"></x-test-class-base>
+    </div>
 </x-tests.app>
