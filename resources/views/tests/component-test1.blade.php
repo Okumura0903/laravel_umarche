41489844ec52a42d0eb414d53e3fd6cb1253ab62
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-15 21:38:15 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,5 @@
     </x-slot>
     コンポーネントテスト１
 
-    <x-tests.card title="タイトル１" content="本文１"></x-tests.card>
+    <x-tests.card title="タイトル１" content="本文１" :message="$message"></x-tests.card>
 </x-tests.app>
