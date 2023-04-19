58a027b7e52316d7c4dbdd96ad3ff1cf73a64331
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-19 21:18:12 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -72,7 +72,7 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message','オーナー登録を実施しました。');
+        ->with(['message'=>'オーナー登録を実施しました。','status'=>'info']);
 
     }
 
@@ -108,7 +108,7 @@ class OwnersController extends Controller
         $owner->save();
         return redirect()
         ->route('admin.owners.index')
-        ->with('message','オーナー情報を更新しました。');
+        ->with(['message'=>'オーナー情報を更新しました。','status'=>'info']);
     }
 
     /**
@@ -117,5 +117,18 @@ class OwnersController extends Controller
     public function destroy(string $id)
     {
         //
+        Owner::findOrFail($id)->delete();//ソフトデリート
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message'=>'オーナー情報を削除しました。','status'=>'alert']);
+    }
+
+    public function expiredOwnerIndex(){
+        $expiredOwners=Owner::onlyTrashed()->get();
+        return view('admin.expired-owners',compact('expiredOwners'));
+    }
+    public function expiredOwnerDestroy($id){
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index');
     }
 }
