eb719627974f1bf9ff33885a606cebaed9063e3c
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-22 21:46:35 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -6,10 +6,17 @@ use InterventionImage;
 class ImageService{
     public static function upload($imageFile,$folderName){
         //            Storage::putFile('public/shops',$imageFile);//リサイズなしの場合
+        //dd($imageFile['image']);
+        if(is_array($imageFile)){//画像を配列で受け取った場合
+            $file=$imageFile['image'];
+        }
+        else{
+            $file=$imageFile;
+        }
         $fileName=uniqid(rand().'_');
-        $extension=$imageFile->extension();
+        $extension=$file->extension();
         $fileNameToStore=$fileName.'.'.$extension;
-        $resizedImage=InterventionImage::make($imageFile)->resize(1920,1080)->encode();
+        $resizedImage=InterventionImage::make($file)->resize(1920,1080)->encode();
         Storage::put('public/'.$folderName.'/'.$fileNameToStore,$resizedImage);
 
         return $fileNameToStore;
