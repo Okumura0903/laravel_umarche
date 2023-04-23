40b5036d62125bbaa4bfdd5cc5b28e44824ee390
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-23 19:14:01 +0900

    category

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers\Owner;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Image;
-use App\Models\product;
-use App\Models\SecondaryCategory;
+use App\Models\Product;
+use App\Models\Shop;
+use App\Models\PrimaryCategory;
 use App\Models\Owner;
 use Illuminate\Support\Facades\Auth;
 
@@ -55,6 +56,15 @@ class ProductController extends Controller
     public function create()
     {
         //
+        $shops=Shop::where('owner_id',Auth::id())
+        ->select('id','name')->get();
+        $images=Image::where('owner_id',Auth::id())
+        ->select('id','title','filename')
+        ->orderBy('updated_at','desc')
+        ->get();
+        $categories=PrimaryCategory::with('secondary')//Eagerローディング
+        ->get();
+        return view('owner.products.create',compact('shops','images','categories'));
     }
 
     /**
