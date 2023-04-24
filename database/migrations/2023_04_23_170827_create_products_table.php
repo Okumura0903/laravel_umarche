3a606a209d002284cbe2c7c34b1896d649eab318
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-24 20:16:56 +0900

    product create

--- database/migrations/2023_04_23_170827_create_products_table.php
+++ database/migrations/2023_04_23_170827_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id') //!!「_id」で終わっている場合はLaravelが自動で判別してくれる（テーブル名不要）
             ->constrained()//外部キー制約
             ->onUpdate('cascade')//外部キーと連動して更新
@@ -24,7 +29,15 @@ return new class extends Migration
             $table->foreignId('image1') //!!「_id」で終わっていない項目を指定
             ->nullable()
             ->constrained('images');//外部キー制約はテーブル名も指定する（自動でないので）
-
+            $table->foreignId('image2') //!!「_id」で終わっていない項目を指定
+            ->nullable()
+            ->constrained('images');//外部キー制約はテーブル名も指定する（自動でないので
+            $table->foreignId('image3') //!!「_id」で終わっていない項目を指定
+            ->nullable()
+            ->constrained('images');//外部キー制約はテーブル名も指定する（自動でないので
+            $table->foreignId('image4') //!!「_id」で終わっていない項目を指定
+            ->nullable()
+            ->constrained('images');//外部キー制約はテーブル名も指定する（自動でないので
             $table->timestamps();
         });
     }
