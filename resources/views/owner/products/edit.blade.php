51e9a69301adb2269dc0fa77d30e1623978b69df
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-04-25 19:52:10 +0900

    constant

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -52,8 +52,8 @@
                                 </div>
                                 <div class="p-2 w-1/2 mx-auto">
                                     <div class="relative flex justify-around">
-                                    <label><input type="radio" name="type" value="1" class="mr-2" checked>追加</label>
-                                    <label><input type="radio" name="type" value="2" class="mr-2">削減</label>
+                                    <label><input type="radio" name="type" value="{{\Constant::PRODUCT_LIST['add']}}" class="mr-2" checked>追加</label>
+                                    <label><input type="radio" name="type" value="{{\Constant::PRODUCT_LIST['reduce']}}" class="mr-2">削減</label>
                                     <x-input-error :messages="$errors->get('type')" class="mt-2" />
                                     </div>
                                 </div>
