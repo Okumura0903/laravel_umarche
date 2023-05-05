58efb16572737261819accc79b08fb7cd179d33d
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-05-05 20:42:36 +0900

    キュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\mail;
 use App\Models\PrimaryCategory;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -31,9 +32,9 @@ class ItemController extends Controller
     }
     //
     public function index(Request $request){
-        Mail::to('test@example.com')
-        ->send(new TestMail());
-
+        //非同期に送信
+        SendThanksMail::dispatch();
+        
         $categories=PrimaryCategory::with('secondary')//Eagerローディング
         ->get();
 
